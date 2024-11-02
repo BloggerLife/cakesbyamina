84f5de24b14d51b1c6b336497b44436af52cbcca
BloggerLife <macarnoldnenyasha@gmail.com>
2024-11-02 07:47:02 +0200

    second

--- src/app/page.js
+++ src/app/page.js
@@ -9,29 +9,40 @@ export default function Home() {
       <Hero />
       <HomeMenu />
       <section className="text-center my-16" id="about">
-        <SectionHeaders
-          subHeader={'Our story'}
-          mainHeader={'About us'}
-        />
+        <SectionHeaders subHeader={"Our story"} mainHeader={"About us"} />
         <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni minima odit recusandae. Illum ipsa non repudiandae? Eum ipsam iste quos suscipit tempora? Aperiam esse fugiat inventore laboriosam officiis quam rem!
+            At Cakes by Amina, we believe in the magic of celebrations and the
+            power of a perfectly baked treat to elevate any moment. Our
+            commitment to quality ingredients, attention to detail, and
+            personalised service ensures that every cake tells a unique story
+            and leaves a lasting impression.
           </p>
-          <p>At consectetur delectus ducimus est facere iure molestias obcaecati quaerat vitae voluptate? Aspernatur dolor explicabo iste minus molestiae pariatur provident quibusdam saepe?</p>
-          <p>Laborum molestias neque nulla obcaecati odio quia quod reprehenderit sit vitae voluptates? Eos, tenetur.</p>
+          <p>
+            Join us on a journey of sweetness and discovery. Explore our gallery
+            of delectable creations, join our community of dessert enthusiasts
+            on social media, and let us be a part of your special moments.
+          </p>
+          <p>
+            Contact Cakes by Amina today to add a touch of sweetness to your
+            life's celebrations.
+          </p>
         </div>
       </section>
       <section className="text-center my-8" id="contact">
         <SectionHeaders
-          subHeader={'Don\'t hesitate'}
-          mainHeader={'Contact us'}
+          subHeader={"Don't hesitate"}
+          mainHeader={"Contact us"}
         />
         <div className="mt-8">
-          <a className="text-4xl underline text-gray-500" href="tel:+46738123123">
-            +46 738 123 123
+          <a
+            className="text-4xl underline text-gray-500"
+            href="tel:+263773799776"
+          >
+            +263 773 799 776
           </a>
         </div>
       </section>
     </>
-  )
+  );
 }
