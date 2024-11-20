75bb6c9a79e3cf7d85398c2c06a66b4dcc8a4975
BloggerLife <macarnoldnenyasha@gmail.com>
2024-11-20 17:29:25 +0200

    addressIn

--- src/components/layout/AddressInputs.js
+++ src/components/layout/AddressInputs.js
@@ -22,19 +22,15 @@ export default function AddressInputs({
         value={streetAddress || ""}
         onChange={(ev) => setAddressProp("streetAddress", ev.target.value)}
       />
-      <div className="grid grid-cols-2 gap-2">
-        <div>
-          <label>City</label>
-          <input
-            disabled={disabled}
-            type="text"
-            placeholder="City"
-            value={city || ""}
-            onChange={(ev) => setAddressProp("city", ev.target.value)}
-          />
-        </div>
-      </div>
-      <label>Country</label>
+      <label>Town</label>
+      <input
+        disabled={disabled}
+        type="text"
+        placeholder="City"
+        value={city || ""}
+        onChange={(ev) => setAddressProp("city", ev.target.value)}
+      />
+      <label>City</label>
       <input
         disabled={disabled}
         type="text"
